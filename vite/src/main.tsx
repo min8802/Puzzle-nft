503c45e8b079afb2cdd9163bb8d1193942be1234
min8802 <ww330@naver.com>
2024-06-11 16:42:25 +0900

    font

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = extendTheme({
+  fonts: {
+    heading: "yeongdeok-sea",
+    body: "yeongdeok-sea",
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
