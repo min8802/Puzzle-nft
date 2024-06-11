d5d537b8486b2b447acf68e00a2647293b0917da
min8802 <ww330@naver.com>
2024-06-11 15:26:10 +0900

    first commit, mint contract, layout setting

---
+++ vite/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react";
+import { FC } from "react";
+
+const Home:FC = () => {
+    return (
+        <Flex>
+            Home
+        </Flex>
+    )
+}
+
+export default Home;
